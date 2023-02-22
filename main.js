451f770f66c3045f15a3761d5924f6bb94530d1a
Diyorbek Abduganiyev <diyorbekabduganiyev@MacBook-Air-Diyorbek.local>
2023-02-22 19:16:56 +0500

    errorr

---
+++ main.js
@@ -0,0 +1,778 @@
+"use strict";
+
+
+
+// ========SWITCH========
+
+
+// TASK 1 =================================================================================================
+
+// var dayNumber = +prompt();
+
+// switch (dayNumber) {
+//     case 1:
+//     console.log("Monday");
+//     break;
+//     case 2:
+//     console.log("Tuesday");
+//     break;
+//     case 3:
+//     console.log("Wednesday");
+//     break;
+//     case 4:
+//     console.log("Thursday");
+//     break;
+//     case 5:
+//     console.log("Friday");
+//     break;
+//     case 6:
+//     console.log("Saturday");
+//     break;
+//     case 7:
+//     console.log("Sunday");
+//     break;
+//     default:
+//     console.log("wrong number!");
+// }
+
+
+// TASK 2 =================================================================================================
+
+// var score = +prompt('k=');
+
+// switch (score) {
+//     case 1:
+//     console.log("Bad");
+//     break;
+
+//     case 2: 
+//     console.log("Unsatisfactory");
+//     break;
+
+//     case 3:
+//     console.log("Satisfactorily");
+//     break;
+
+//     case 4:
+//     console.log("Good");
+//     break;
+
+//     case 5:
+//     console.log("Excelent");
+//     break;
+
+//     default:
+//     console.log("It is not a score number!!!");
+// }
+
+
+
+
+
+// TASK 3 =================================================================================================
+
+// var monthNum = +prompt("n=")
+
+
+// switch (monthNum) {
+//     case 1:
+//     console.log("winter");
+//     break;
+
+//     case 2:
+//     console.log("winter");
+//     break;
+
+//     case 3:
+//     console.log("winter");
+//     break;
+
+//     case 4:
+//     console.log("spring");
+//     break;
+
+//     case 5:
+//     console.log("spring");
+//     break;
+
+//     case 6:
+//     console.log("spring");
+//     break;
+
+//     case 7:
+//     console.log("summer");
+//     break;
+
+//     case 8:
+//     console.log("summer");
+//     break;
+
+//     case 9:
+//     console.log("summer");
+//     break;
+
+
+//     case 10:
+//     console.log("automn");
+//     break;
+
+//     case 11:
+//     console.log("automn");
+//     break;
+
+//     case 12:
+//     console.log("automn");
+//     break;
+
+//     default:
+//     console.log("we have only 12 month");
+// }
+
+
+
+// TASK 4 =================================================================================================
+
+
+
+// var monthNum = +prompt("n=")
+
+
+// switch (monthNum) {
+//     case 1:
+//     console.log("There are 30 days in this month");
+//     break;
+
+//     case 2:
+//     console.log("There are 31 days in this month");
+//     break;
+
+//     case 3:
+//     console.log("There are 30 days in this month");
+//     break;
+
+//     case 4:
+//     console.log("There are 29 days in this month");
+//     break;
+
+//     case 5:
+//     console.log("There are 30 days in this month");
+//     break;
+
+//     case 6:
+//     console.log("There are 30 days in this month");
+//     break;
+
+//     case 7:
+//     console.log("There are 31 days in this month");
+//     break;
+
+//     case 8:
+//     console.log("There are 31 days in this month");
+//     break;
+
+//     case 9:
+//     console.log("There are 31 days in this month");
+//     break;
+
+
+//     case 10:
+//     console.log("There are 30 days in this month");
+//     break;
+
+//     case 11:
+//     console.log("There are 31 days in this month");
+//     break;
+
+//     case 12:
+//     console.log("There are 29 days in this month");
+//     break;
+
+//     default:
+//     console.log("we have only 12 month");
+// }
+
+
+// TASK 5 =================================================================================================
+
+
+
+// var a =+prompt('a=');
+// var b =+prompt('b=');
+// var c =prompt('c=');
+
+// switch (c) {
+//     case '+':
+//     console.log(a + b);
+//     break;
+
+//     case '-':
+//     console.log(a - b);
+//     break;
+
+//     case '/':
+//     console.log(a / b);
+//     break;
+
+//     case '*':
+//     console.log(a * b);
+//     break;
+
+//     default:
+//         console.log('It is not math operator');
+//     break;
+// }
+
+
+// TASK 6 =================================================================================================
+
+
+
+// var score = +prompt('k=');
+
+// switch (score) {
+//     case 1:
+//     console.log("100$");
+//     break;
+
+//     case 2: 
+//     console.log("200&");
+//     break;
+
+//     case 3:
+//     console.log("300$");
+//     break;
+
+//     case 4:
+//     console.log("400$");
+//     break;
+
+//     case 5:
+//     console.log("500$");
+//     break;
+
+//     default:
+//     console.log("It is not a score number!!!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ========FOR ( )========
+
+
+
+// TASK 1 =================================================================================================
+
+
+// let k = 1;
+// let n = +prompt("n=");
+
+// // while (k < n) {
+// //     console.log("appeared"+k);
+// //     k++;
+// // }
+
+
+// for (let k = 1; k < n; k++) {
+//     console.log("appeared");
+// }
+
+
+// TASK 2 =================================================================================================
+
+
+// let k = +prompt("k=");
+// let n = +prompt("n=");
+
+// while (k <= n) {
+//     console.log(k);
+//     k++;
+// }
+
+// TASK 3 =================================================================================================
+
+
+// let a = 10;
+// let b = 1;
+
+// while (a > b) {
+//     a--;
+//     if(a != b){
+//         console.log(a);
+//     }
+// }
+
+// TASK 4 =================================================================================================
+
+// let a = 1
+// let b = 10
+// let c = 10000
+
+// while (a <= b) {
+//     console.log(a);
+//     if (a == 1){
+//         console.log(a + 'kg', c + ' sum');
+//     }else if (a==2){
+//         console.log(a + 'kg', c*a + 'sum' ); 
+//     }else if (a==3){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }else if (a==4){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }else if (a==5){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }else if (a==6){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }else if (a==7){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }else if (a==8){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }else if (a==9){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }else if (a==10){
+//         console.log(a + 'kg', c*a + 'sum' );
+//     }
+//     a++;
+// }
+
+
+// TASK 5 =================================================================================================
+
+
+
+// let a = 1
+// let b = 10
+// let c = 1000
+
+// while (a <= b) {
+//     if (a == 1){
+//         console.log('0.1kg ' + c + ' s um');
+//     }else if (a==2){
+//         console.log('0.2kg ' + c*a + ' sum' ); 
+//     }else if (a==3){
+//         console.log('0.3kg ' + c*a + ' sum' );
+//     }else if (a==4){
+//         console.log('0.4kg ' + c*a + ' sum' );
+//     }else if (a==5){
+//         console.log('0.5kg ' + c*a + ' sum' );
+//     }else if (a==6){
+//         console.log('0.6kg ' + c*a + ' sum' );
+//     }else if (a==7){
+//         console.log('0.7kg ' + c*a + ' sum' );
+//     }else if (a==8){
+//         console.log('0.8kg ' + c*a + ' sum' );
+//     }else if (a==9){
+//         console.log('0.9kg ' + c*a + ' sum' );
+//     }else if (a==10){
+//         console.log('1kg ' + c*a + ' sum' );
+//     }
+//     a++;
+// }
+
+
+
+
+// TASK 6 =================================================================================================
+
+
+// let a = 1
+// let b = 10
+// let c = 1000
+
+// while (a <= b) {
+//     if (a == 1){
+//         console.log('1.2kg ' + c*1.2 + ' sum');
+//     }else if (a==2){
+//         console.log('1.4kg ' + c*1.4 + ' sum' ); 
+//     }else if (a==3){
+//         console.log('1.6kg ' + c*1.6 + ' sum' );
+//     }else if (a==4){
+//         console.log('1.8kg ' + c*1.8 + ' sum' );
+//     }else if (a==5){
+//         console.log('2kg ' + c*2 + ' sum' );
+//     }
+//     a++;
+// }
+
+
+// TASK 7 =================================================================================================
+
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+// let sum = 0
+
+// for (let i = a; a <= b; a++) {
+//     sum += a
+// }
+
+// console.log(sum);
+
+
+// TASK 8 =================================================================================================
+
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+// let multiple = 1
+
+// for (let i = a; a <= b; a++) {
+//    multiple *= a
+// }
+
+// console.log(multiple);
+
+
+// TASK 9 =================================================================================================
+
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+// let multiple = 0
+
+// for (let i = a; a <= b; a++) {
+//     multiple += a**2
+// }
+
+// console.log(multiple);
+
+
+// TASK 10 =================================================================================================
+
+// O'xshamadi
+
+
+
+// TASK 11 =================================================================================================
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+
+// while (a < b) {
+//     a++
+//     if (a%2 ==1) {
+//         console.log(a);
+//     }
+// }
+
+
+
+// TASK 12 =================================================================================================
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+
+// while (a < b) {
+//     a++
+//     if (a%2 ==0) {
+//         console.log(a);
+//     }
+// }
+
+
+
+// TASK 13 =================================================================================================
+
+
+// nima qilish kerakligini chuna olmadim
+
+
+// TASK 14 =================================================================================================
+
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+
+// while (a < b) {
+//     a++
+//     console.log(a**2);
+// }
+
+
+
+// TASK 15 =================================================================================================
+
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+
+// while (a>b) {
+//     a--
+//     console.log(a);
+// }
+
+
+// TASK 16 =================================================================================================
+
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+
+
+// while (a<b) {
+//     a++
+//     if (a%5 !=0) {
+//         console.log(a);
+//     }
+// }
+
+
+// TASK 17 =================================================================================================
+
+
+// let a = 1
+// let b = +prompt('b=')
+// let multiple = 1
+
+// for (let i = a; a <= b; a++) {
+//    multiple *= a
+// }
+
+// console.log(multiple);
+
+// TASK 20 =================================================================================================
+
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+// let x = +prompt('x=')
+
+// while (a<b) {
+//     a++
+//     if (a%x ==0) {
+//         console.log(a);
+//     }
+// }
+
+
+// TASK 21 ===============================================================================================
+
+// let x = +prompt('x=')
+// let a = +prompt('a=')
+// let sum = 0
+// let n = +prompt('n=')
+// let nN = +prompt('nN=')
+// let multipleSum = 1
+
+
+//     for (let i = x; i <= a; i++){
+//         sum += i
+//     }
+
+// for (let i = n; i <= nN; i++) {
+//     multipleSum += i * sum
+// }
+
+// console.log(multipleSum);
+
+
+// TASK 24 ===============================================================================================
+
+// let a = +prompt('a=')
+// let b = 1
+// let c = 10
+
+// for (let i = b; i <= c; i++) {
+//     console.log(a + '*' + i + '=' + a * i);
+// }
+
+
+// TASK 25 ===============================================================================================
+
+// let a = +prompt('a=')
+// let b = +prompt('b=')
+
+
+// for (let i = a; i < b; i++) {
+//     if (a != i && b != i) {
+//         console.log(i);
+//     }
+// }
+
+
+// ========FUNCTION========
+
+
+
+// TASK 1 ===============================================================================================
+
+// let n=Number(prompt('n='))
+// function pow () {
+//     console.log(n**3);
+// }
+
+// pow()
+
+
+
+// TASK 2 ===============================================================================================
+
+// let a=+prompt('a=')
+// let b =+prompt('b')
+
+// function sum () {
+//   console.log(a+b);
+// }
+// sum()
+
+// TASK 3 ===============================================================================================
+
+
+// let a=+prompt('a=')
+// let b =+prompt('b')
+
+// function sum () {
+//   console.log(a-b);
+// }
+// sum()
+
+
+
+// TASK 4 ===============================================================================================
+
+
+// let n=Number(prompt('n='))
+// function pow () {
+//     console.log(n**2);
+//     console.log(n**3);
+//     console.log(n**4);
+// }
+
+// pow()
+
+
+// TASK 5 ===============================================================================================
+
+
+
+
+// let n=+prompt('n')
+
+// function odd () {
+//  for (let i = 0; i < n; i++) {
+//   if(i%2===1){
+//     console.log(i);
+//   }
+//  }
+// }
+// odd()
+
+
+
+
+// TASK 6 ===============================================================================================
+
+
+
+
+// let n=+prompt('n')
+
+// function odd () {
+//  for (let i = 0; i < n; i++) {
+//   if(i%2===0){
+//     console.log(i);
+//   }
+//  }
+// }
+// odd()
+
+
+
+
+// TASK 7 ===============================================================================================
+
+
+
+
+// let n =+prompt('n')
+// let x=+prompt('x')
+
+// function increment () {
+//   console.log(++n);
+// }
+// increment()
+
+// function dicrement () {
+//   console.log(--x);
+// }
+// dicrement()
+
+
+
+
+// TASK 8 ===============================================================================================
+
+
+
+
+
+// let n = Number(prompt("Enter a number: "));
+
+// function number () {
+//   for(let i = 1; i<=n; i++){
+//           if(n % i == 0){
+//               console.log(i);
+//           }
+//       }
+// }
+// number()
+
+
+
+// TASK 9 ===============================================================================================
+
+
+
+
+// function Math (parameters) {
+//     let a = +prompt("a=");
+//     let b = +prompt("b=");
+//     let MathOperator = prompt("MathOperator=");
+//     switch (MathOperator) {
+//         case "+":
+//         console.log(`a+b=${a + b}`);
+//         break;
+//         case "-":
+//         console.log(`a-b=${a - b}`);
+//         break;
+//         case "*":
+//         console.log(`a*b=${a * b}`);
+//         break;
+//         case "/":
+//         console.log(`a/b=${a / b}`);
+//         break;
+//         default:
+//         console.error("is not a MathOperator");
+//     }
+// }
+
+// Math()
+
+
+
+// TASK 10 ===============================================================================================
+
+// function multiply () {
+//     let a = +prompt('a=')
+//     let b = 1
+//     let c = 10
+    
+//     for (let i = b; i <= c; i++) {
+//         console.log(a + '*' + i + '=' + a * i);
+//     }
+    
+// }
+
+// multiply()
